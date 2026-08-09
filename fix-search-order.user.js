00628d3ad9164bbbfa7776eb993367799658f71f
Paul <8686328+paulte@users.noreply.github.com>
2026-08-09 15:21:26 +0100

    Adding v2.2.2 version of fix-search-order.user.js for greasyfork.org integration

--- fix-search-order.user.js
+++ fix-search-order.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ocado Auto Price Sort
-// @version      2.2.1
-// @released     2026-08-09T15:11:59+0100
+// @version      2.2.2
+// @released     2026-08-09T15:21:26+0100
 // @namespace    https://github.com/paulte/tampermonkey-ocado
 // @description  Automatically sets Ocado sort to "Price per Unit: Low to High" when searching without an explicit sort.
 // @author       paulte
